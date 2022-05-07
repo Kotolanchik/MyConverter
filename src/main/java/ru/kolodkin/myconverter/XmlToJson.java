9bd81a3f667f48508326217716517b5ea48c95c4
Konstantin Kolodkin <kostya.kolodkin@inbox.ru>
2022-05-07 16:20:18 +0300

    json2xml

--- src/main/java/ru/kolodkin/myconverter/XmlToJson.java
+++ src/main/java/ru/kolodkin/myconverter/XmlToJson.java
@@ -20,6 +20,46 @@ import java.util.List;
 
 public class XmlToJson {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        Document document = readXml("input.xml");
+
+        List<String> allFirm = new ArrayList<>();
+        List<Ram> ramList = new ArrayList<>();
+        List<Rams> rams = new ArrayList<>();
+
+        getRamListFromXml(document, allFirm, ramList);
+        jsonModelRam(allFirm, ramList, rams);
+
+        writeJson("output.json", rams);
+    }
+
+    public static void jsonModelRam(List<String> allFirm, List<Ram> ramList, List<Rams> rams) {
+        for (int i = 0; i < allFirm.size(); i++) {
+            Rams buffRams = new Rams();
+            buffRams.setFirm(allFirm.get(i));
+            rams.add(buffRams);
+        }
+
+        for (int i = 0; i < rams.size(); i++) {
+            for (int j = 0; j < ramList.size(); j++) {
+                if (ramList.get(j).getFirm().equals(rams.get(i).getFirm())) {
+                    rams.get(i).getRam().add(new Ram(
+                            ramList.get(j).getIdRam(),
+                            ramList.get(j).getTitle(),
+                            ramList.get(j).getReleaseYear(),
+                            ramList.get(j).getSpecifications()));
+                }
+            }
+        }
+    }
+
+    public static void writeJson(String output, List<Rams> rams) throws IOException {
+        Root root = new Root(rams);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(output), root);
+    }
+
+    public static Document readXml(String path) throws ParserConfigurationException, IOException, SAXException {
         // Получение фабрики, чтобы после получить билдер документов.
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
@@ -29,14 +69,13 @@ public class XmlToJson {
         // Запарсили XML, создав структуру Document. Теперь у нас есть доступ ко всем элементам, каким нам ну84884жно.
         Document document = documentBuilder.parse(new File("input.xml"));
         document.getDocumentElement().normalize();
+        return document;
+    }
 
+    public static void getRamListFromXml(Document document, List<String> allFirm, List<Ram> ramList) {
         Element ramsElement = (Element) document.getElementsByTagName("rams").item(0);
-
-        // Получение списка всех элементов employee внутри корневого элемента (getDocumentElement возвращает ROOT элемент XML файла).
         NodeList ramNodeList = document.getDocumentElement().getElementsByTagName("ram");
-        List<String> allFirm = new ArrayList<>();
 
-        List<Ram> ramList = new ArrayList<>();
         for (int i = 0; i < ramNodeList.getLength(); i++) {
             if (ramNodeList.item(i).getNodeType() == Node.ELEMENT_NODE) { // проверка типа
                 Element ramElement = (Element) ramNodeList.item(i);
@@ -95,50 +134,8 @@ public class XmlToJson {
             }
         }
 
-        ramList.forEach(System.out::println);
-        System.out.println("\n\n\n");
-
-        List<Rams> rams = new ArrayList<>();
-
         allFirm = allFirm.stream().distinct().toList();
-
-
-        for (int i = 0; i < allFirm.size(); i++) {
-            Rams buffRams = new Rams();
-            buffRams.setFirm(allFirm.get(i));
-            rams.add(buffRams);
-        }
-
-        for (int i = 0; i < rams.size(); i++) {
-            for (int j = 0; j < ramList.size(); j++) {
-                if (ramList.get(j).getFirm().equals(rams.get(i).getFirm())) {
-                    rams.get(i).getRam().add(new Ram(
-                            ramList.get(j).getIdRam(),
-                            ramList.get(j).getTitle(),
-                            ramList.get(j).getReleaseYear(),
-                            ramList.get(j).getSpecifications()));
-                }
-            }
-        }
-
-        Root root = new Root(rams);
-        ObjectMapper mapper = new ObjectMapper();
-        String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
-
-        mapper.writerWithDefaultPrettyPrinter().writeValue(new File("outpuTTTt.json"), root);
-        System.out.println(jsonString);
     }
 
-    public static Document readXml(String path) throws ParserConfigurationException, IOException, SAXException {
-        // Получение фабрики, чтобы после получить билдер документов.
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-        // Получили из фабрики билдер, который парсит XML, создает структуру Document в виде иерархического дерева.
-        DocumentBuilder documentBuilder = factory.newDocumentBuilder();
-
-        // Запарсили XML, создав структуру Document. Теперь у нас есть доступ ко всем элементам, каким нам ну84884жно.
-        Document document = documentBuilder.parse(new File("input.xml"));
-        document.getDocumentElement().normalize();
-        return document;
-    }
 }
